1592064a6248cf8a82528895e9204434489a4076
Ruben Feng <rubenfeng12@gmail.com>
2025-07-02 15:11:24 -0500

    ✨ feat: Implementado firestore data base y ejemplo de uso

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 import { Link, useRouter } from "expo-router";
 import { Pressable, Text, View,StyleSheet } from "react-native";
 import { useState } from "react";
+import { app } from "@/firebase";
 
 export default function Index() {
+  const db = getFirestore(app)
   const [pressed, setPressed] = useState<boolean>(false)
   const route = useRouter();
+  const guardarAlgo = async() =>{
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Alan",
+        middle: "Mathison",
+        last: "Turing",
+        born: 1912
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+  const obenerAlgo = async() =>{
+    try{
+      const response = await getDocs(collection(db, "users"))
+      response.forEach(item=>console.log(item.id, item.data()))
+    } catch(e){
+      console.error(e);
+    }
+  }
   
   return (
     <View
@@ -23,6 +48,24 @@ export default function Index() {
           onPressOut={() => {setPressed(false); route.push("/login")}}  
         > 
           <Text style={{color: pressed?"blue": "#ffffff"}}>LogOut</Text>
+        </Pressable>
+
+        <Pressable
+          style={style.Button}
+          onPress={guardarAlgo}
+        >
+          <Text style={{color:"white"}}>
+            Prueba guardar algo
+          </Text>
+        </Pressable>
+
+        <Pressable
+          style={style.Button}
+          onPress={obenerAlgo}
+        >
+          <Text style={{color:"white"}}>
+            Prueba leer algo
+          </Text>
         </Pressable>
       
     </View>
